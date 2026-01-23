f60cd1ec8da9f8bfbec8068221867ca06161284f
cnatiu2 <cnatiu2@wgu.edu>
2026-01-23 10:50:29 -0800

    Cleaned up the App.java main method. Moved JFrame stuff to it's own class

--- src/App.java
+++ src/App.java
@@ -1,34 +1,16 @@
-import java.awt.BorderLayout;
-import java.awt.Font;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import resources.Colors;
-import resources.FontLoader;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        App app = new App();
+        app.start();
+    }
 
-        Font titleFont = FontLoader.titleFont(24f);
-        JFrame frame = new JFrame();
-        frame.setSize(1280,720);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(true);
-
-        JPanel layoutPanel = new JPanel(new BorderLayout());
-        JPanel titlePanel = new JPanel();
-
-        titlePanel.setBackground(Colors.SOLID_LIGHT_BLUE);
-        layoutPanel.add(titlePanel, BorderLayout.NORTH);
-        
-
-        JLabel bankTitleText = new JLabel("NATIUK BANK");
-        bankTitleText.setFont(titleFont);
-        titlePanel.add(bankTitleText);
-
-        frame.add(layoutPanel);
-        layoutPanel.setBackground(Colors.SOLID_CELESTE);
-
-        frame.setVisible(true);
+    private void start() {
+        try {
+            Homepage homepage =  new Homepage();
+        } catch (Exception e) {
+            System.err.println("Failed to start");
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/Homepage.java
@@ -0,0 +1,36 @@
+import java.awt.BorderLayout;
+import java.awt.Font;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import resources.Colors;
+import resources.FontLoader;
+
+public class Homepage {
+
+    public Homepage() {
+
+        Font titleFont = FontLoader.titleFont(24f);
+    
+        JFrame frame = new JFrame();
+        frame.setSize(1280,720);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(true);
+
+        JPanel layoutPanel = new JPanel(new BorderLayout());
+        JPanel titlePanel = new JPanel();
+
+        titlePanel.setBackground(Colors.SOLID_LIGHT_BLUE);
+        layoutPanel.add(titlePanel, BorderLayout.NORTH);
+        
+
+        JLabel bankTitleText = new JLabel("NATIUK BANK");
+        bankTitleText.setFont(titleFont);
+        titlePanel.add(bankTitleText);
+
+        frame.add(layoutPanel);
+        layoutPanel.setBackground(Colors.SOLID_CELESTE);
+
+        frame.setVisible(true);
+    }
+}
